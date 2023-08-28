55da7a4be1bad16d17b2a209c416c4df6be01217
Aily Gucfa <126049721+AilyGucfa@users.noreply.github.com>
2023-08-28 20:28:14 +0000

    created 4 tables for instagram data modelling. will continue later

--- src/models.py
+++ src/models.py
@@ -1,32 +1,98 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+import datetime
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'users'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column (String(250), unique = True, nullable = False)
+    password = Column(String(250), nullable=False)
+    email =  Column (String(250), unique = True, nullable = False)
+    full_name =  Column (String(250), nullable = False)
+    bio  = Column (String(300),nullable = True)
+    profile_picture = Column(String)
+    post = relationship ('Post', back_populates = 'user')
 
-class Address(Base):
-    __tablename__ = 'address'
+    def serialize (self):
+        return{
+            'user_name' : self.user_name,
+            'email' : self.email,
+            'full_name' : self. full_name,
+            'bio' : self.bio,
+            'profile_picture' : self.profile_picture
+        }
+   
+class Post(Base):
+    __tablename__ = 'posts'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column (Integer, ForeignKey('users.id'))
+    image_url = Column (String(150), nullable = False)
+    caption = Column (String(300), nullable = True)
+    post_time = Column(DateTime, default = datetime.datetime.utcnow)
+    user = relationship('User' ,back_populates = 'post')
+    comment = relationship ('Comment', back_populates = 'user')
+
 
     def to_dict(self):
-        return {}
+        return {
+            'user_id' : self.user_id,
+            'image_url' : self.image_url,
+            'caption': self. caption,
+            'post_time': self.post_time
+        }
+    
+
+class Comment (Base):
+    __tablename__ = 'comments'
+    id = Column (Integer , primary_key = True)
+    user_id = Column (Integer, ForeignKey('users.id'))
+    post_id = Column (Integer, ForeignKey('posts.id'))
+    description_comment = Column (String (250), nullable = False)
+    comment_time = Column (DateTime , default = datetime.datetime.utcnow)
+    user = relationship ('User')
+    post = relationship ('Post', back_populates = 'comment')
+    like = relationship ('Like', back_populates ='user')
+
+    def serialize (self):
+        return{
+            'user_id': self.user_id,
+            'post_id': self.post_id,
+            'description_comment': self.description_comment,
+            'comment_time': self.comment_time
+        }
+
+
+
+class Like (Base):
+    __tablename__ = 'likes'
+    id = Column (Integer, primary_key = True)
+    user_id = Column (Integer , ForeignKey ('users.id'))
+    post_id = Column (Integer, ForeignKey('posts.id'))
+    count_likes = Column (Integer)
+    like_time = Column (DateTime, default = datetime.datetime.utcnow)
+    post = relationship ('Post' , back_populates = 'like')
+    user = relationship ('User')
+    
+
+    def serialize (self):
+        return{
+            'user_id': self.user_id,
+            'post_id' : self.post_id,
+            'count_likes': self.count_likes,
+            'like_time' : self.like_time
+        }
+
+
 
 ## Draw from SQLAlchemy base
 try:
